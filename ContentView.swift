9196f7f64543b77a569c19a230b0bb6be45cd609
Ariel Suarez <arielsm3@gmail.com>
2023-06-10 07:31:21 -0400

    Add files via upload
    This Project is Based on the Challenge from Hacking with SwiftUI - Milestone: Projects 4-6 (Day 35) by Paul Hudson.
    I fixed some major bugs preventing the app from working as intended. 
    I may also add some animation, in the future.

--- ContentView.swift
+++ ContentView.swift
@@ -1,148 +1,238 @@
 //
 //  ContentView.swift
-//  StackingUpButtons
+//  Edutainment
+//
+//  Created by Ariel David Suarez on 5/6/23.
 //
 
 /*
- * Based on the Hacking with Swift project GuessTheFlag by Paul Hudson
- * with some minor modifications.
+ * This Project is Based on the Challenge from Hacking with SwiftUI - Milestone: Projects 4-6 (Day 35) by Paul Hudson
  */
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
+    
+    @State private var multiplier = 2 // Initial value of 2, it is bound to Stepper
+    @State private var multiplicant = 1 // Value of multiplicant will be 1 at the beginning of game
+    
+    @State private var numberOfQuestions = 5
+    @State private var remainingQuestions = 5
     @State private var score = 0
     
-    @State private var finalScoreTitle: String = ""
-    @State private var showingFinalScore: Bool = false
-    
-    @State private var countries = ["Russia", "Nigeria", "Poland", "Spain", "Estonia", "France", "Germany", "Ireland", "Italy", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var hasStarted: Bool = false // Indicates if game has started or not
+    @State private var gameStops: Bool = false // Track whether game has started and trigger stop game alert
         
-    var body: some View {
-        ZStack {
-            LinearGradient(stops: [
-                .init(color: .yellow, location: 0.5),
-                .init(color: .black, location: 0.5),
-            ], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .ignoresSafeArea()
+    @State private var answer: Int = 0 // Represents answer of multiplication
+    @State private var answerTitle: String = ""
+    @State private var showingAnswer: Bool = false
+    
+    @State private var correctMessage = ""
+    
+    @State private var gameLevel = 1
+    
+    @State private var messageOfQuestion: String = ""
+    
+    func startGame() {
+        hasStarted = true
+        gameStops = false
+    }
+    
+    func stopGame() { // Stop game and prompt user for confirmation
+        gameStops = true
+        hasStarted = false
+    }
+    
+    // struct 'Question' to create variable type with which to populate Array 'questions'
+    struct Question {
+        let text: String
+        var answer: Int
+    }
+    
+    // MARK: Populate questions array with values for its properties, this fuction may need to be turned into a variable
+    var assignQuestions: Question {
+        
+        let level: Int = gameLevel
+        var questions: Array<Question> = []
+        
+        // Level 1: Three different types of questions so far, range: 2...12
+        if level < 2 {
+            questions.append(Question(text: "What is \(multiplicant) x \(multiplier)?", answer: multiplicant * multiplier))
             
-            VStack(spacing: 15) {
-                Spacer()
-                Text("Guess The Flag")
-                    .font(.largeTitle.bold())
-                    .foregroundColor(.white)
-                
-                VStack(spacing: 15) {
-                    VStack {
-                        Text("Tap the flag of:")
-                            .font(.subheadline.weight(.heavy))
-                            .foregroundColor(.primary)
-                        
-                        Text(countries[correctAnswer])
-                            .font(.largeTitle.weight(.semibold))
-                            .foregroundStyle(.secondary)
+            //questions.append(Question(text: "What is the product of \(multiplierNumber) x \(multiplierNumber)?", answer: multiplierNumber * multiplierNumber))
+        }
+        
+        // Level 2: Consider using 3 variables instead of 2, range: perhaps, extend to higher numbers (e.g. 5...15)?
+        if level >= 2 {
+            questions.append(Question(text: "How much is \(multiplier) x \(multiplicant)?", answer: multiplier * multiplicant))
+        }
+        
+        // Level 3: Consider using an extra Int variable in order to ask the product of three different numbers
+        
+        return questions[Int.random(in: 0..<questions.count)] // Return a random element in the array of Questions
+    }
+    
+    
+    var body: some View {
+        let colorRadient = LinearGradient(gradient: Gradient(colors: [
+            Color(red: 0.7, green: 0.7, blue:0.9),
+            Color(red: 0, green: 0.7, blue: 0.7),
+            Color(red: 0.3, green: 0.5, blue: 0.7),
+            Color(red: 0.9, green: 0.3, blue: 0.3)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        
+        VStack {
+            Text(hasStarted ? "Stop Game?" : "Start Game?")
+                .font(.title.weight(.bold))
+                .foregroundColor(.purple)
+            
+            Spacer()
+            
+            Text("Remaining questions: \(remainingQuestions)")
+                .foregroundColor(.red)
+                .font(.largeTitle)
+                .opacity(hasStarted ? 1 : 1)
+            
+            Spacer()
+            
+            VStack {
+                // MARK: Start Game Button
+                Button {
+                    if !hasStarted {
+                        remainingQuestions = numberOfQuestions
+                        hasStarted = true
                     }
-                    .padding()
-                    ScrollView {
-                        ForEach(0..<3) { number in
-                            Button {
-                                flagTapped(number)
-                                
-                            } label: {
-                                FlagImage(countries: countries, number: number)
-                                    .clipShape(Capsule())
-                                    .shadow(radius: 5)
+                    else if hasStarted {
+                        stopGame()
+                    }
+                    
+                } label: {
+                    Text(hasStarted ? "Stop" : "Start") // if true, game has not started yet
+                        .foregroundColor(hasStarted ? .red : .blue)
+                }
+                .alert("Do you want to stop the game?", isPresented: $gameStops) {
+                    // MARK: Ask for confirmation to stop game
+                    Button("Yes", action: {
+                         // Restart game -> Reset questions to zero
+                    })
+                    Button("No", action: {
+                        // Resume game from where player left off
+                        hasStarted = true
+                    })
+                }
+                
+                Text(hasStarted ? "Game Started!" : "Press Start!")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Text(assignQuestions.text)
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.purple)
+                
+                Spacer()
+                
+                // MARK: Promting user for input answer
+                HStack {
+                    Text("Enter Answer: ")
+                        .multilineTextAlignment(.center)
+                    TextField("Answer", value: $answer, format: .number)
+                        .disabled(!hasStarted) // Disable if game has not started
+                }
+                .foregroundColor(.white)
+                    .font(.largeTitle.weight(Font.Weight.bold))
+                    .multilineTextAlignment(.center)
+                
+                // MARK: Check Answer Button
+                Button {
+                    checkAnswer()
+                } label: {
+                    Text("Check")
+                        .padding()
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                        .background(Color(red:0.85, green: 0.85, blue: 0.85))
+                        .opacity(hasStarted ? 1 : 0)
+                        
+                }
+                .disabled(!hasStarted) // Disable check button until game starts
+                .alert(answerTitle, isPresented: $showingAnswer) {
+                    // Continue, randomly change value of multiplicant according to level
+                    if remainingQuestions > 1 {
+                        Button("Ok") {
+                            
+                            if gameLevel == 1 {
+                                multiplicant = Int.random(in: 0...12)
+                            } else if gameLevel >= 2 {
+                                multiplicant = Int.random(in: 5...15)
                             }
                         }
+                    } else {
+                        HStack {
+                            Text("\(answerTitle)")
+                            Button("Start again?", action: {
+                                restart()
+                            })
+                        }
                     }
-                    .padding()
                     
-                    HStack {
-                        Button("Reshuffle", action: {
-                            reshuffle()
-                        })
-                        Button("New Game", action: {
-                            newGame()
-                        })
-                        .foregroundColor(.red)
-                    }
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 20)
-                .background(.opacity(0.3))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                Spacer()
-                Spacer()
-                Text("Score: \(score)") // "Score ???"
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-                Spacer()
-            }
-            .padding()
-        }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            Text("Your score is \(score)")
-        }
-        .alert(finalScoreTitle, isPresented: $showingFinalScore) {
-            Button("New Game", action: newGame)
-        }
-        
-    }
-       
-    func flagTapped(_ number: Int) {
-        if score < 8 { // Score: 8 will be reached right after it marks 7
-            if number == correctAnswer {
-                scoreTitle = "Correct"
-                score += 1
-            }
-            else {
-                scoreTitle = "Wrong! That's the flag of \(countries[number])"
-                score -= 1
+                .padding()
             }
             
-            showingScore = true
+            VStack {
+                Text("Settings:")
+                    .font(.largeTitle)
+                Stepper(value: $multiplier, in: 2...12) {
+                    Text("Multiply table of: \(multiplier)")
+                }
+                
+                Stepper(value: $numberOfQuestions, in: 5...20, step: 5) {
+                    Text("Number of questions: \(numberOfQuestions)")
+                }
+                
+                Stepper(value: $gameLevel, in: 1...3) {
+                    Text("Game level: \(gameLevel)")
+                }
+            }
+            .foregroundColor(.white)
+            .font(.title)
+            .disabled(hasStarted) // Disable Stepper once game starts
+            .opacity(hasStarted ? 0 : 1) // Render invisible once game starts
+
+            Spacer()
         }
-        else {
-            showingFinalScore = true
-            finalScoreTitle = "Congratulations! Your score is \(score)"
+        .padding()
+        .background(colorRadient)
+    }
+    
+    // MARK: Modify so questions.text matches questions.answer from populateQuestions func
+    func checkAnswer() {
+        if remainingQuestions > 1 {
+            // If user input answer is equal to answer of multiplication question
+            if answer == assignQuestions.answer {
+                score += 1
+                answerTitle = "Correct!"
+            } else {
+                answerTitle = "Wrong! The Answer is \(multiplier * multiplicant)."
+            }
+            remainingQuestions -= 1
+        } else {
+            // Last question for this game
+            restart()
         }
+        showingAnswer = true // Trigger alert
+        //multiplicant = Int.random(in: 2...12)
     }
     
-    func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
-    }
     
-    func reshuffle() {
-        countries.shuffle()
-        if !countries.contains(correctAnswer.description) {
-            countries.shuffle()
-        }
-    }
     
-    func newGame() {
-        score = 0 // Reset score to 0
-        countries.shuffle()
+    func restart() {
+        // MARK: Function to handle game resets after last question is answered
+        remainingQuestions = numberOfQuestions
     }
-    
 }
 
-struct FlagImage: View {
-    var countries: Array<String>
-    var number: Int
-    
-    var body: some View {
-        Image(countries[number])
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
+struct Content_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
---
+++ EdutainmentApp.swift
@@ -0,0 +1,34 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Ariel David Suarez on 5/6/23.
+//
+
+/*
+ 1. Stepper
+ 2. DatePicker, displayedComponents
+ 3. Date, DateComponents
+ 4. Machine Learning
+ 5. List
+ 6. onAppear()
+ 7. Bundle
+ 8. fatalError()
+ 9. UITextChecker
+ 10. implicit animation
+ 11. customized animation
+ 12. animation modifiers
+ 13. explicit animation
+ 
+ */
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
